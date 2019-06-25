80a29050d00eb8d359c702af74d2b5e4003a36db
sezenack <46613116+sezenack@users.noreply.github.com>
2019-06-25 14:49:25 -0400

    update Schedule Web Scraper

--- Schedule Web Scraper.py
+++ Schedule Web Scraper.py
@@ -8,7 +8,7 @@ url1 = 'https://rpiathletics.com/schedule.aspx?path=hockey&'
 response1 = requests.get(url1, headers={'User-Agent': 'Custom'})
 soup1 = BeautifulSoup(response1.text, 'html.parser')
 table1 = soup1.findAll('span')
-#table1 = table1[0:37]
 infostrings1 = []
 for t in table1:
     infostrings1.append(t.get_text())
+infostrings1 = infostrings1[0:578]
